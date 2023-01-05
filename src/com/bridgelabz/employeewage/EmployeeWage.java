ac49a7e053d2c82bc8f18c8c1f78079f1ab8294c
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-05 21:19:19 +0530

    Welcomme to monthly employee wage.

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,31 +3,43 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
     static final int HOURS_PER_DAY = 8;
-    static final int DAYS_PER_MONTH = 8;
+    static final int PART_TIME_HOURS = 8;
+    static final int DAYS_PER_MONTH = 20;
+    static final int HOURS_PER_MONTH = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
-        int employeecheck = (int) (Math.random() *10) %3;
-        System.out.println(employeecheck);
-        int dailyWage = 0;
+        int hoursscount = 0;
         int monthlywage = 0;
-        switch (employeecheck){
-            case 1:
-                System.out.println("Employee is present");
-                dailyWage = WAGE_PER_HOUR * HOURS_PER_DAY;
-                monthlywage = dailyWage * DAYS_PER_MONTH;
-                break;
+        int day = 1;
+        while (hoursscount<=HOURS_PER_MONTH && day <= DAYS_PER_MONTH) {
+            int employeecheck = (int) (Math.random() * 10) % 3;
+            int dailyWage = 0;
+            System.out.println(day);
+            switch (employeecheck) {
+                case 1:
+                    System.out.println("Employee is present");
+                    dailyWage = WAGE_PER_HOUR * HOURS_PER_DAY;
+                    hoursscount += HOURS_PER_DAY;
+                    break;
 
-            case 2:
-                System.out.println("Employee is working Part time for day");
-                dailyWage = WAGE_PER_HOUR * (HOURS_PER_DAY / 2);
-                monthlywage = dailyWage * DAYS_PER_MONTH;
-                break;
-
-            default:
-                System.out.println("employee is absent");
+                case 2:
+                    System.out.println("Employee is working Part time for day");
+                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+                    hoursscount += PART_TIME_HOURS;
+                    break;
+                default:
+                    System.out.println("employee is absent");
+            }
+            monthlywage += dailyWage;
+            day++;
+            System.out.println("employee get the monthly wage til th day " + day + " is " + monthlywage);
+            System.out.println("Hours till the day " + hoursscount);
+            System.out.println();
         }
-        System.out.println("employee get the daily wage is " + dailyWage);
+        System.out.println();
+        System.out.println("Working days in month " + day);
+        System.out.println("Working hours in month " + hoursscount);
         System.out.println("employee get the monthly wage is " + monthlywage);
     }
 }
